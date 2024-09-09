a500c8bc0d264ad5d3f05ed8d629577a7372604f
Om1965 <loshko@ya.ru>
2024-09-09 12:09:30 +0300

    Add files via upload

---
+++ main_3.py
@@ -0,0 +1,6 @@
+count_dz = 12
+count_hours = 1.5
+name_kurs = 'Python'
+time_one_dz = count_hours / count_dz
+
+print('Курс:', name_kurs,', всего задач:', count_dz,', затрачено часов:',count_hours, ', среднее время выполнения ', time_one_dz,' часа', sep='')
